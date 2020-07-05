d2e576c58415fde869a11045dd12142b17a1953e
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-07-05 19:48:41 -0300

    Validações com yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
 const User = require('../models/User')
+const Yup = require('yup')
 
 class UserController {
     async store(req, res) {
+        const Schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6)
+        })
+
+        if(!(await Schema.isValid(req.body))){
+            return res.status(401).json({ error: `Falha na validação` })
+        }
+
         const { email, name, password, provider } = req.body
 
         if(!email || !password ) {
@@ -24,9 +35,46 @@ class UserController {
         return res.status(200).json({ success: "User created" })
     }
 
-    updateUser(req, res) {
-        console.log(req.userId)
-        return res.json({ message: "Chegamos até aqui" })
+    async updateUser(req, res) {
+        const Schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().min(6)
+              .when('oldPassword', (oldPassword, field) =>
+                  oldPassword ? field.required() : field 
+              )
+        })
+
+        if(!(await Schema.isValid(req.body))) {
+            return res.status(401).json({ error: `Falha na validação` })
+        }
+
+        const { email, oldPassword } = req.body
+
+        const user = await User.findByPk(req.userId)
+
+        if(email != user.email) {
+
+            const users = await User.findOne({ where: { email } })
+            if(users) { 
+                return res.status(401).json({ error: "Email Já existe" })
+            }
+        }
+
+       if(oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ error: "Senha Incorreta" })
+       }
+
+       const { id, name, provider } = await user.update(req.body)
+
+       return res.status(200).json({
+           id,
+           email,
+           provider,
+           name
+       })
+
     }
 }
 
