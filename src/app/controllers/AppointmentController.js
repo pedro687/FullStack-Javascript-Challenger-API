13bbc5a894950a2344cee9a04f82a7c6504045ac
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-07-08 23:05:01 -0300

    fazendo verificações de DATAS

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 const Appointments = require('../models/Appointment')
+const {startOfHour, parseIso, isBefore} = require('date-fns')
 const Yup = require('yup')
 const User = require('../models/User')
 
@@ -13,6 +14,7 @@ class AppointmentController {
             return res.status(401).json({ error: "Validation Fails" })
         }
 
+
         const { date, provider_id } = req.body
 
         const Providers = await User.findOne({
@@ -21,6 +23,30 @@ class AppointmentController {
 
         if(!Providers) {
             return res.status(401).json({ error: "Provider does not exists" })
+        }
+
+        /*
+        *check for past dates
+        */
+
+        const hourStart = startOfHour(parseIso(date))
+        if(isBefore(hourStart, new Date())) { 
+            return res.statu(401).json({error: "past date not are permited" })
+        }
+
+        /* check for availibility
+        */
+
+        const checkAvailability = await Appointments.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart
+            }
+        })
+
+        if(checkAvailability) {
+            return res.status(400).json({ error: "appointment date is not availability" })
         }
 
 
