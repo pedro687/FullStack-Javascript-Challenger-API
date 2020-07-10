323ec446ad0aeb98fc0b708d58e4e44fd5c0166a
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2020-07-10 00:46:06 -0300

    Configurando MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -3,12 +3,15 @@ const databaseConfig = require('../config/database')
 const User = require('../app/models/User')
 const Files = require('../app/models/Files')
 const Appointment = require('../app/models/Appointment')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const Models = [User, Files, Appointment]
 
 class Database {
     constructor() {
         this.init()
+        this.mongo()
     }
 
     init() {
@@ -17,8 +20,20 @@ class Database {
         Models
          .map(model => model.init(this.connection))
         
-         Models.map(model => model.association && model.association(this.connection.models))        
-        
+         Models.map(model => model.association && model.association(this.connection.models))           
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(process.env.DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => { 
+            console.log("Database Connected")
+        })
+        .catch((error) => {
+             console.log(`Database Error Because -> ${error}`)
+        })
     }
 }
 
